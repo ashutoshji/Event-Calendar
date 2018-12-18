8878e04a74f592ab433fb11159c8c6379989e136
ashutoshji <ashutoshkumar22@gmail.com>
2018-12-18 11:07:01 +0800

    file changed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+import Scheduler from 'devextreme-react/scheduler';
+import axios from 'axios';
+import {CSVLink} from 'react-csv';
+
+import * as AspNetData from 'devextreme-aspnet-data-nojquery';
+
+const url = 'https://js.devexpress.com/Demos/Mvc/api/SchedulerData';
+const dataSource = AspNetData.createStore({
+  key: 'AppointmentId',
+  loadUrl: `${url }/Get`,
+  insertUrl: `${url }/Post`,
+  updateUrl: `${url }/Put`,
+  deleteUrl: `${url }/Delete`,
+  onBeforeSend(_, ajaxOptions) {
+    ajaxOptions.xhrFields = { withCredentials: true };
+  }
+});
+
+const currentDate = new Date();
+const views = ['day', 'workWeek', 'month'];
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      data:[]
+    }
+  }
+
+  componentDidMount(){
+     axios.get(url + '/Get').then(res=>
+      {this.setState({data:res.data.data})})
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="scheduler-app">
+      <div className="exportToCsv">
+      <CSVLink asyncOnClick={true} data={this.state.data}>Export to CSV</CSVLink>
       </div>
+      <Scheduler
+        dataSource={dataSource}
+        views={views}
+        defaultCurrentView={'day'}
+        defaultCurrentDate={currentDate}
+        height={600}
+        startDayHour={9}
+        endDayHour={19}
+        textExpr= {'Text'}
+        startDateExpr={'StartDate'}
+        endDateExpr={'EndDate'}
+        allDayExpr={'AllDay'} />
+        </div>
     );
   }
 }
